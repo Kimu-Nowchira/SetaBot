574f3cad02db070ce2eaabeb669e3d70306a02d7
키뮤 <47320945+Kimu-Nowchira@users.noreply.github.com>
2021-01-27 09:03:13 +0900

    first commit

---
+++ config.py
@@ -0,0 +1,44 @@
+'''
+    <config.py>
+    봇의 설정을 이렇게 따로 빼 놓으면 편하게 설정할 수 있어요!
+    - 키뮤 제작(0127 버전)
+'''
+
+
+class Config:
+    # 봇의 토큰을 넣어 주세요
+    token = 'ODAyOTM2MzQ5MjM3NzA2Nzkz.YA2evw.1QRQGRMQemlnvWNle7xlLfuGllk'
+    # 디버그 모드가 켜졌을 때 사용할 토큰을 넣어주세요(위와 똑같이 넣어도 괜찮아요)
+    test_token = 'ODAyOTM2MzQ5MjM3NzA2Nzkz.YA2evw.1QRQGRMQemlnvWNle7xlLfuGllk'
+
+    # 디버그 여부
+    is_debug = False
+
+    # 하고 있는 게임 (프로필에 '... 하는 중'으로 보이는 것)
+    activity = "키뮤가 최고라고 말"
+
+    # 접두사(명령어 앞에 붙여야 하는 것)
+    # '~야' 같이 뒤의 명령어와 띄어서 써야 하는 접두사는 뒤를 한 칸 띄어 주세요! (예시의 '세타야 ')
+    # 접두사는 여러 개를 지정할 수 있어요.
+    prefixes = ['!', '세타야 ']
+
+    # 봇의 버전을 입력해 주세요. 테스트 명령어에서 쓴답니다.
+    version = '1.0'
+
+    def using_token(self):
+        '''
+        사용해야 하는 봇 토큰을 반환합니다.
+        '''
+        if self.is_debug:
+            return self.test_token
+        else:
+            return self.token
+
+    def prefixes_no_space(self):
+        '''
+        접두사들을 띄어쓰기 없이 반환합니다.
+        '''
+        result = []
+        for i in self.prefixes:
+            result.append(i.replace(' ', ''))
+        return result
---
+++ utils/level_design.py
@@ -0,0 +1,54 @@
+'''
+    <level_design.py>
+    게임 봇을 만드시는 분들을 위한 간단한 레벨 디자인을 제공해요.
+
+    ※ 파이썬 초심자라면 이 파일을 수정하지 않는 것을 추천드려요!
+    - 키뮤 제작(0127 버전)
+'''
+# 기본 레벨 디자인
+# n 레벨에서 n+1 레벨이 되기 위해 필요한 경험치 = 10n
+# n 레벨이 되기 위해 필요한 총 경험치 5n^2+5n
+
+
+def exp_to_level(exp: int):
+    '''누적 경험치값을 레벨로 전환시켜줍니다. 음수 경험치는 레벨 0으로 처리됩니다.'''
+    if exp < 0:
+        return 0
+    else:
+        return int(((exp/5)+(1/4))**0.5 + 0.5)
+
+
+def level_to_atk(level: int):
+    '''레벨 -> 공격력'''
+    return int(10*(level**1/2))
+
+
+def level_to_def(level: int):
+    '''레벨 -> 방어력'''
+    return int(10*(level**1/2))
+
+
+def level_to_maxhp(level: int):
+    '''레벨 -> 최대 HP'''
+    return int(25*(level**1/2))
+
+
+def level_to_maxmp(level: int):
+    '''레벨 -> 최대 MP'''
+    return int(20*(level**1/2))
+
+
+def exp_to_atk(exp: int):
+    return level_to_atk(exp_to_level(exp))
+
+
+def exp_to_def(exp: int):
+    return level_to_def(exp_to_level(exp))
+
+
+def exp_to_maxhp(exp: int):
+    return level_to_maxhp(exp_to_level(exp))
+
+
+def exp_to_maxmp(exp: int):
+    return level_to_maxmp(exp_to_level(exp))
